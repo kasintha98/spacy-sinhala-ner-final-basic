e10b206d32b1aa53144a715be0f5938528bd63cb
Kasintha Kalhara <39892786+kasintha98@users.noreply.github.com>
2023-02-01 14:28:36 +0000

    new

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api
 from check_entities import ExtractEntities
+import json
+import requests
 
 # create an instance of flask
 app = Flask(__name__)
@@ -16,9 +18,20 @@ class CheckEntities(Resource):
             text = request.json['text']
             extracted_gazetteer_entities = ExtractEntities.tag_gazetteer_entities_from_text(text)
             extracted_fuzzy_entities = ExtractEntities.tag_fuzzy_entities_from_text(text)
-            extracted_model_entities = ExtractEntities.tag_model_entities_from_text(text)
+            extracted_spacy_model_entities = ExtractEntities.tag_model_entities_from_text(text)
+            extracted_bert_model_entities = {}
+
+            # get BERT model predictions
+            text_for_bert = {
+                "text": text
+                }
+            res = requests.post(url="http://localhost:6000/check-entities", json=text_for_bert)
+
+            if res and res.status_code == 200:
+                extracted_bert_model_entities = res.json()["extractedEntitiesFromModel"]
+
             # return a json response
-            return make_response(jsonify({'text': text, 'extractedGazetteerEntities': extracted_gazetteer_entities, 'extractedFuzzyEntities': extracted_fuzzy_entities, 'extractedEntitiesFromModel':extracted_model_entities}), 200)
+            return make_response(jsonify({'text': text, 'extractedGazetteerEntities': extracted_gazetteer_entities, 'extractedFuzzyEntities': extracted_fuzzy_entities, 'extractedEntitiesFromSpacyModel':extracted_spacy_model_entities, 'extractedEntitiesFromBertModel': extracted_bert_model_entities}), 200)
         else:
             return {'error': 'Request must be JSON'}, 400
 
